6d1d69c79434ba0f917459d264d2d647d1e3fb46
Rian Girard <riangirard@gmail.com>
2021-05-13 17:07:34 -0700

    more updates

--- users_with_bank_accounts.py
+++ users_with_bank_accounts.py
@@ -4,30 +4,33 @@ class User:
         self.email = email_address
         self.accounts = [BankAccount(account_name="checking", int_rate=0.02, balance=0), BankAccount(account_name="savings", int_rate=0.04, balance=0)]
 
-    def make_deposit(self, account_name, amount):       
-        for i in self.accounts:
-            if i.account_name == account_name:
-                i.deposit(amount)
+    def make_deposit(self, account_name, amount):       # make_deposits takes in an account name
+        for i in self.accounts:                         # for i in the list of self.accounts
+            if i.account_name == account_name:          # if i.account_name (from BankAccount object) == account_name input value
+                i.deposit(amount)                       # then have record "i" call the "deposit" method and put that $$$ in the account 
         return self
 
     def make_withdrawal(self, account_name, amount):       
-        if account_name == "checking":
-            self.accounts[0].withdraw(amount)
-            return self
-        if account_name == "savings":
-            self.accounts[1].withdraw(amount)
-            return self
+        for i in self.accounts:                         # same, see above
+            if i.account_name == account_name:
+                i.withdraw(amount)
+        return self
 
     def display_user_balance(self, account_name):
-        if account_name == "checking":
-            print(account_name, self.accounts[0].display_account_info())
-        if account_name == "savings":
-            print(account_name, self.accounts[1].display_account_info())
+        for i in self.accounts:                         # same, see above
+            if i.account_name == account_name:
+                print(i.account_name, i.display_account_info())
         return self
 
 ### NEW !!! 
-    def create_account(self, account_name):
-        self.accounts.append(BankAccount(account_name=account_name))
+    def create_account(self, account_name, balance):
+        self.accounts.append(BankAccount(account_name=account_name, balance=balance))
+        return self
+
+    def show_balances(self):
+        for i in self.accounts:
+            print(i.account_name, i.int_rate, i.balance)
+        print('\n', '*'*80)
         return self
 ### 
 
@@ -35,10 +38,7 @@ class BankAccount:
     def __init__(self, account_name, int_rate=0.02, balance=0):
         self.int_rate = int_rate
         self.account_name = account_name
-        if balance == None:
-            self.balance = 0
-        else: 
-            self.balance = balance
+        self.balance = balance
 
     def deposit(self, amount):
         self.balance += amount
@@ -57,5 +57,8 @@ guido.make_deposit("checking", 100)
 guido.make_deposit("savings", 50)
 guido.make_withdrawal("checking", 50)
 guido.make_withdrawal("savings", 45)
-guido.display_user_balance("checking")
-guido.display_user_balance("savings")
+
+guido.create_account("college fund", 0)
+guido.show_balances()
+guido.create_account("rainy day", 100)
+guido.show_balances()
